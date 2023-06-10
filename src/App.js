acebe320d4d5b31e3a98fd1f2a5873c3c8318849
Swapnil <swapnilshah102002@gmail.com>
2023-06-10 19:47:44 +0530

    added projects page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Bitly from "./components/Bitly"
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import LoadingBar from 'react-top-loading-bar';
 import About from "./pages/About";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
       <Route exact path="/tdm" element={<Videogrid key = " tdm" playlist_id = "PLc6bUwTVKwCM07hH4vbznPPr2K68Ji_YR" max = "8"/>}> </Route>
       <Route exact path="/drills" element={<Videogrid key = "drills" playlist_id = "PLc6bUwTVKwCMfjMH3feuA0eGYabjU6A75" max = "30"/>}> </Route>
       <Route exact path="/about" element = {<About/>}> </Route>
+      <Route exact path="/projects" element = {<Projects/>}> </Route>
       <Route path = "/:url" element = {<Bitly/>}></Route>
       </Routes>
     </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,46 +11,51 @@ function Navbar() {
     <>
       <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark h-25">
         <div className="container-fluid">
-          <Link className="navbar-brand " to="/">   
-          <img src = {process.env.PUBLIC_URL  + '/images/profile.png'} 
-           className="d-inline-block align-top" alt="" width="40" height="40" />         
+          <Link className="navbar-brand " to="/">
+            <img src={process.env.PUBLIC_URL + '/images/profile.png'}
+              className="d-inline-block align-top" alt="" width="40" height="40" />
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
+              <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/">SilentkillerOP</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/">Home</Link>
               </li>
-              
+
               <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/shorts">Shorts</Link>
               </li>
-              
+
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/tdm">Tdm</Link> 
+                <Link className="nav-link" aria-current="page" to="/tdm">Tdm</Link>
               </li>
-              
+
               <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/drills">Drills</Link>
               </li>
               <li className="nav-item">
                 <Link className="nav-link" aria-current="page" to="/about">About me</Link>
               </li>
-              
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" to="/projects">Projects</Link>
+              </li>
+
+
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Social Medias
+                  Social Medias
                 </a>
                 <ul className="dropdown-menu  bg-dark text-white" aria-labelledby="navbarDropdown">
                   <li><a className="dropdown-item bg-dark text-white " target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/ig_silentkiller_op/">Instagram</a></li>
+
                   <li >
-                <a className="dropdown-item  bg-dark text-white" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@silentkillerop" >YouTube</a>
-              </li>
+                    <a className="dropdown-item  bg-dark text-white" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@silentkillerop" >YouTube</a>
+                  </li>
                   <li><a className="dropdown-item  bg-dark text-white" target="_blank" rel="noopener noreferrer" href="https://twitter.com/swapnilshah102">Twitter</a></li>
                   <li><hr className="dropdown-divider" /></li>
                   <li><a className="dropdown-item  bg-dark text-white" href="/">Nothing else here</a></li>
