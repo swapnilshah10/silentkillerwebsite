2876753640d8bbe68ce76e989349fa27ae78613e
Swapnil <swapnilshah102002@gmail.com>
2024-02-12 10:06:49 +0530

    bitly page working

---
+++ src/app/[bitly]/page.js
@@ -0,0 +1,155 @@
+"use client"
+import React, { useEffect } from "react";
+// import { useParams } from "react-router-dom";
+import axios from "axios";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import styles from './page.module.css';
+
+// let background_url = http://localhost:3000/ + "/background.jpg";
+var myStyle = {
+  backgroundImage: `url(${"http://localhost:3000//backgroundd.gif"})`,
+  height: "100%",
+  minHeight: "100vh",
+  backgroundSize: "cover",
+  backgroundRepeat: "repeat-y",
+  padding: "10px",
+  margin: "0px",
+  border: " 10px solid black",
+  color: "white",
+
+};
+
+function Bitly({ params }) {
+  // console.log(background_url);
+  // const router = useRouter()
+  let url = params.bitly;
+  const [data, setData] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const [shortUrl, setShortUrl] = React.useState(null);
+  const [longUrl, setLongUrl] = React.useState(null);
+  const [ishort, setIshort] = React.useState(false);
+
+  const fetchData = async () => {
+    axios
+      .get(`https://swapnil123.pythonanywhere.com/api/${url}/`)
+      .then((response) => {
+        window.location = response.data.url;
+      })
+      .catch((error) => {
+        setError(error);
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleSubmit = () => {
+    let data = {
+      long_url: longUrl,
+    };
+    if (ishort) {
+      data.short_url = shortUrl;
+    }
+
+    axios
+      .post(`https://swapnil123.pythonanywhere.com/api/payal/`, data)
+      .then((response) => {
+        console.log(response);
+        setData("https:silentkillerop.live/" + response.data.url);
+      });
+  };
+  return (
+    <div style={myStyle}>
+      <div>{error && <div>{url.toUpperCase()} DOESNT EXIST</div>}</div>
+      <div style={{
+        dispaly: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh"
+      }}>
+        <div className="container d-flex flex-column justify-content-center align-item-center h-75">
+          <div className="row justify-content-center border-white">
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-header bg-dark text-white border-white">Shorten URL</div>
+                <div className="card-body bg-dark text-white">
+                  <div>
+                    <input
+                      placeholder="Enter URL to shorten"
+                      type="text"
+                      name="long_url"
+                      className="form-control bg-dark text-white border-white"
+                      onChange={(e) => {
+                        setLongUrl(e.target.value);
+                      }}
+                      style={styles.input}
+                      required
+                    />
+                  </div>
+                  {ishort && (
+                    <div className="my-2">
+                      <input
+                        type="text"
+                        name="short_url"
+                        placeholder="Enter short word"
+                        className="form-control bg-dark text-white border-white"
+                        style={styles.input}
+                        onChange={(e) => {
+                          setShortUrl(e.target.value);
+                        }}
+                        required
+                      />
+                    </div>
+                  )}
+                  <div className="form-check bg-dark text-white">
+                    <input
+                      className="form-check-input bg-dark text-white border-white"
+                      type="checkbox"
+                      value=""
+                      id="flexCheckDefault"
+                      onClick={() => {
+                        setIshort(!ishort);
+                      }}
+                    />
+                    <label
+                      className="form-check-label bg-dark text-white"
+                      // for="flexCheckDefault"
+                      style={{ color: "black" }}
+                    >
+                      Create custom URL
+                    </label>
+                  </div>
+                  <button
+                    type="submit"
+                    className="btn btn-dark my-2 border-white"
+                    onClick={handleSubmit}
+                  >
+                    Shorten
+                  </button>
+                </div>
+              </div>
+
+              {data && (
+                <div className="card my-4" style={{ backgroundColor: "#90eea8" }}>
+                  <div className="card-body d-flex">
+                    <a href={data.substring(25)} target="_blank" style={{ color: "black" }} >
+                      {data}
+                    </a>
+                    <CopyToClipboard text={data}>
+                      <button className=" mx-4 btn btn-outline-dark btn-sm position-relative top-0 start-0" onClick={{}}>Copy </button>
+                    </CopyToClipboard>
+                  </div>
+                </div>
+              )}
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Bitly;
---
+++ src/components/Navbar.js
@@ -0,0 +1,107 @@
+import React from "react";
+import Link from 'next/link'
+// import { useState } from "react"
+
+// import  { useEffect} from "react";
+// import axios from "axios";
+// const fetchData = () => {
+//   axios
+//     .get(`https://swapnil123.pythonanywhere.com/save_ip/`)
+// };
+function Navbar() {
+//   useEffect(() => {
+//     fetchData();
+//   }, []);
+  // const [inputText, setInputText] = useState("");
+  // let inputHandler = (e) => {
+  //   var lowerCase = "search?q="+e.target.value.toLowerCase();
+  //   setInputText(lowerCase);
+  // };
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark h-25">
+        <div className="container-fluid">
+          <Link className="navbar-brand " href="/">
+            <img src={'http://localhost:3000/profile.png'}
+              className="d-inline-block align-top" alt="" width="40" height="40" />
+          </Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" href="/">SilentkillerOP</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" href="/">Home</Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" href="/shorts">Shorts</Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" href="/tdm">Tdm</Link>
+              </li>
+
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" href="/drills">Drills</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" href="/chess">Chess</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" href="/about">About me</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" aria-current="page" href="/projects">Projects</Link>
+              </li>
+
+
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Social Medias
+                </a>
+                <ul className="dropdown-menu  bg-dark text-white" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item bg-dark text-white " target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/ig_silentkiller_op/">Instagram</a></li>
+
+                  <li >
+                    <a className="dropdown-item  bg-dark text-white" target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@silentkillerop" >YouTube</a>
+                  </li>
+                  <li><a className="dropdown-item  bg-dark text-white" target="_blank" rel="noopener noreferrer" href="https://twitter.com/swapnilshah102">Twitter</a></li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item  bg-dark text-white" href="/">Nothing else here</a></li>
+                  
+                </ul>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Coding
+                </a>
+                <ul className="dropdown-menu  bg-dark text-white" aria-labelledby="navbarDropdown">
+                    <li>
+                    <a className="dropdown-item  bg-dark text-white" target="_blank" rel="noopener noreferrer" href="/resume" >Resume</a>
+                    <li><a className="dropdown-item  bg-dark text-white" target="_blank" rel="noopener noreferrer" href="/leetcode">LeetCode</a></li>
+                    <li><a className="dropdown-item  bg-dark text-white" target="_blank" rel="noopener noreferrer" href="/codechef">Codechef</a></li>
+                    <li><a className="dropdown-item  bg-dark text-white" target="_blank" rel="noopener noreferrer" href="/codingninjas">CodingNinjas</a></li>
+                  </li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li><a className="dropdown-item  bg-dark text-white" href="/">Nothing else here</a></li>
+                </ul>
+              </li>
+            </ul>
+            {/* <form className="d-flex">
+              <input className="form-control me-2  bg-dark text-white " type="search" placeholder="Search" onChange={inputHandler}  aria-label="Search" />
+              <Link className="" to= {inputText} >
+              <button className="btn btn-outline-success" type="submit">Search</button>
+              </Link>
+            </form> */}
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
